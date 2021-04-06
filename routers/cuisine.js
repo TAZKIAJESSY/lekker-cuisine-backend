22ed8ca1ed6ae21a06df46d9a61494778398c870
tazkiajessy <jessy566eee@gmail.com>
2021-04-06 13:56:51 +0200

    make endpoint for all cuisine

---
+++ routers/cuisine.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const Cuisine = require("../models").cuisine;
+const Ingredient = require("../models").ingredient;
+
+const router = new Router();
+
+// get all cusines
+
+router.get("/cuisines", async (req, res, next) => {
+  try {
+    const allCuisines = await Cuisine.findAll({ include: [Ingredient] });
+    if (!allCuisines) {
+      res.status(404).send("Cuisines not found");
+    } else {
+      res.send(allCuisines);
+    }
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+module.exports = router;
