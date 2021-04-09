721b0a920237b3c7ecbacc3948433fedbb121c9d
tazkiajessy <jessy566eee@gmail.com>
2021-04-09 18:57:37 +0200

    favourite router created

---
+++ routers/favourite.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const Cuisine = require("../models").cuisine;
+const User = require("../models").user;
+
+const router = new Router();
+
+module.exports = router;
