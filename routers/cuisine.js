2a542bfb019c7e4b6ed16fb9ab4b976d2f47370e
tazkiajessy <jessy566eee@gmail.com>
2021-04-14 21:23:49 +0200

    change in cuisine router

--- routers/cuisine.js
+++ routers/cuisine.js
@@ -39,30 +39,6 @@ router.get("/", async (req, res, next) => {
 
 //http GET :4000/cusines
 
-//update a like button for cuisine
-router.patch("/likes/:id", async (req, res, next) => {
-  // if (!cuisine.userId === req.user.id) {
-  //   return res
-  //     .status(403)
-  //     .send({ message: "You are not authorized to like cuisine" });
-  // }
-
-  try {
-    const id = parseInt(req.params.id);
-
-    const cuisine = await Cuisine.findByPk(id);
-    const likes = cuisine.likes;
-
-    const updatedCuisine = await cuisine.update({ likes: likes + 1 });
-
-    return res.status(200).send(updatedCuisine);
-  } catch (e) {
-    next(e.message);
-  }
-});
-
-//http PATCH :4000/cuisines/likes/2
-
 //cuisine details
 router.get("/:id", async (req, res, next) => {
   try {
@@ -165,5 +141,29 @@ router.post("/", authMiddleware, async (req, res, next) => {
     next(e);
   }
 });
+
+//delete a cuisine for a user
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const user = req.user;
+    const cuisine = await Cuisine.findByPk(id);
+
+    if (cuisine.userId === user.id) {
+      const deletedRecipe = await cuisine.destroy();
+      res.status(201).send({ message: "Cuisine deleted", cuisine });
+    } else {
+      return res
+        .status(400)
+        .send("You are not authorized to delete this cuisine");
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+// http POST :4000/login email=jessy@hi.com password=jessy1234
+// http GET :4000/cuisines/id(cuisineIdpk) Authorization:"Bearer token"
 
 module.exports = router;
